dfec1726d1be7043d9b0b7520931a2ccfb6c63ec
ccquiambao <ccquiambao@gmail.com>
2017-10-04 18:05:48 -0700

    cv w/ probability calibration pipeline completed, reliability curve plotting started

--- model_builer.py
+++ model_builer.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.linear_model import LogisticRegression
-from sklearn.calibration import CalibratedClassifierCV
+from sklearn.calibration import CalibratedClassifierCV, calibration_curve
 from sklearn.model_selection import KFold
 import matplotlib.pyplot as plt
 from collections import defaultdict
@@ -47,18 +47,69 @@ def data_preprocessing(df, target_col='Incident_Cat', drop_cat=False):
 
     return y, df_final
 
+
 def add_scores(score_dict, model_key, y_true, y_pred, predicted_probs):
-    l_loss = log_loss(y_true, predict_probs)
+    l_loss = log_loss(y_true, predicted_probs)
     brier_loss = brier_score_loss(y_true, predicted_probs)
     f1 = f1_score(y_true, y_pred)
 
     for key, val in [('log_loss', l_loss),
                      ('brier_loss', brier_loss),
                      ('f1', f1)]:
-
+        print 'Adding {} for {}'.format(key, model_key)
         score_dict[model_key][key].append(val)
 
     return score_dict
+
+
+def prob_calibration_cross_val(model, X_train, y_train, n_folds=5):
+    '''
+    cross validate, includes probaility calibration
+    '''
+    model_names = ['base', 'isotonic', 'sigmoidal']
+    score_dict = {key : defaultdict(list) for key in model_names}
+
+    # TODO split into prob calibration and cross validation pipeline
+    kfold = KFold(n_splits=n_folds)
+
+    ind = 0
+    for train_index, test_index in kfold.split(X_train):
+        ind += 1
+        print 'Fold {}'.format(ind)
+        X_training, X_val = X_train[train_index], X_train[test_index]
+        y_training, y_val = y_train[train_index], y_train[test_index]
+
+        print 'Fitting base model'
+        model.fit(X_train, y_train)
+
+        # TODO functionalize predicts, predict_probs
+        # TODO functionalize sigmoid fit and isotonic fit
+        predicts = model.predict(X_val)
+        predict_probs = model.predict_proba(X_val)[:, 1]
+        print 'Updating score dictionary with base scores'
+        score_dict = add_scores(score_dict, 'base', y_val, predicts, predict_probs)
+
+        # isotonic
+        print 'Fitting isotonic calibration'
+        iso = CalibratedClassifierCV(model, method='isotonic', cv=3)
+        iso.fit(X_train, y_train)
+        iso_preds = iso.predict(X_val)
+        iso_probs = iso.predict_proba(X_val)[:, 1]
+        print 'updating score dict w/ isotonic scores'
+        score_dict = add_scores(score_dict, 'isotonic', y_val, iso_preds, iso_probs)
+
+        # sigmoid / Platt Scaling
+        print 'Fitting sigmoidal calibration'
+        sigmoid = CalibratedClassifierCV(model, method='sigmoid', cv=3)
+        sigmoid.fit(X_train, y_train)
+        sigmoid_preds = sigmoid.predict(X_val)
+        sigmoid_probs = sigmoid.predict_proba(X_val)[:, 1]
+        print 'updating score dict w/ sigmoid scores'
+        score_dict = add_scores(score_dict, 'sigmoidal', y_val, sigmoid_preds, sigmoid_probs)
+
+
+    return score_dict
+
 
 if __name__ == '__main__':
     # if len(sys.argv) < 2:
@@ -83,48 +134,14 @@ if __name__ == '__main__':
     pipe = Pipeline([('scaler', StandardScaler()),
                      ('model', rf)])
 
-    kfold = KFold(n_splits=5)
+    score_dict = prob_calibration_cross_val(pipe, X_train, y_train)
 
-    model_names = ['base', 'isotonic', 'sigmoidal']
-    score_dict = {key : defaultdict(list) for key in model_names}
-
-    ind = 0
-    for train_index, test_index in kfold.split(X_train):
-        ind += 1
-        print 'Fold {}'.format(ind)
-        X_training, X_val = X_train[train_index], X_train[test_index]
-        y_training, y_val = y_train[train_index], y_train[test_index]
-
-        print 'Fitting base model'
-        pipe.fit(X_train, y_train)
-        predicts = pipe.predict(X_val)
-        predict_probs = pipe.predict_proba(X_val)[:, 1]
-        print 'Updating score dictionary with base scores'
-        score_dict = add_scores(score_dict, 'base', y_val, predicts, predict_probs)
-
-        # isotonic
-        print 'Fitting isotonic calibration'
-        iso = CalibratedClassifierCV(pipe, method='isotonic', cv=3)
-        iso.fit(X_train, y_train)
-        iso_preds = iso.predict(X_val)
-        iso_probs = iso.predict_proba(X_val)[:, 1]
-        print 'updating score dict w/ isotonic scores'
-        score_dict = add_scores(score_dict, 'isotonic', y_val, iso_preds, iso_probs)
-
-        # sigmoid / Platt Scaling
-        print 'Fitting sigmoidal calibration'
-        sigmoid = CalibratedClassifierCV(pipe, method='sigmoid', cv=3)
-        sigmoid.fit(X_train, y_train)
-        sigmoid_preds = sigmoid.predict(X_val)
-        sigmoid_probs = sigmoid.predict_proba(X_val)[:, 1]
-        print 'updating score dict w/ sigmoid scores'
-        score_dict = add_scores(score_dict, 'sigmoidal', y_val, sigmoid_preds, sigmoid_probs)
-
-    print score_dict
-    print 'finished cross validation'
+    # fit rf to whole data, score, print feature importances
+    # TODO functionalize, shouldn't be using test set yet
 
     pipe.fit(X_train, y_train)
     test_score = pipe.score(X_test, y_test)
+    print '\n\n'
     print 'test_score: {}'.format(test_score)
 
     feat_imp = pipe.named_steps['model'].feature_importances_
@@ -132,10 +149,66 @@ if __name__ == '__main__':
 
     col_fi = sorted(zip(cols, feat_imp), key = lambda x: x[1])
 
+    print '\n\n'
+    print 'Feature Importances: '
     for col, fi, in col_fi:
         print col, fi
-    # import data
-    # process data
-    # cross validate
-    # train model
-    # train calibration
+
+
+    # construct reliability curve
+    # TODO create calibration set
+    # TODO functionalize
+    # TODO move to different script
+
+
+
+
+
+    plt.figure(figsize=(12,8))
+    # base probs
+    base_probs = pipe.predict_proba(X_test)[:,1]
+    base_pred = pipe.predict(X_test)
+    base_pos_frac, base_mean_predict = (
+        calibration_curve(y_test, base_probs, n_bins=10)
+                                       )
+
+    plt.plot(base_mean_predict, base_pos_frac, 'o-', label='base')
+
+    # Platt scaling
+    sigmoid = CalibratedClassifierCV(pipe, method='sigmoid', cv=10)
+    sigmoid.fit(X_train, y_train)
+    sigmoid_probs = sigmoid.predict_proba(X_test)[:, 1]
+    sigmoid_preds = sigmoid.predict(X_test)
+    sigmoid_pos_frac, sigmoid_mean_predict = (
+        calibration_curve(y_test, sigmoid_probs, n_bins=10)
+                                             )
+
+    plt.plot(sigmoid_mean_predict, sigmoid_pos_frac, 's-', label='sigmoid')
+
+    # isotonic scaling
+    iso = CalibratedClassifierCV(pipe, method='isotonic', cv=10)
+    iso.fit(X_train, y_train)
+    iso_probs = iso.predict_proba(X_test)[:, 1]
+    iso_preds = iso.predict(X_test)
+    iso_pos_frac, iso_mean_predict = (
+        calibration_curve(y_test, iso_probs, n_bins=10)
+                                             )
+
+    plt.plot(iso_mean_predict, iso_pos_frac, 's-', label='isotonic')
+
+    # plot perfrect line
+    perfect = np.linspace(0, 1, 10)
+    plt.plot(perfect, perfect, '--', label='perfect')
+
+
+    model_names = ['base', 'isotonic', 'sigmoidal']
+    score_dict = {key : defaultdict(list) for key in model_names}
+
+    score_dict = add_scores(score_dict, 'base', y_test, base_pred, base_probs)
+    score_dict = add_scores(score_dict, 'sigmoidal', y_test, sigmoid_preds, sigmoid_probs)
+    score_dict =  add_scores(score_dict, 'isotonic', y_test, iso_preds, iso_probs)
+
+    for key, value in score_dict.iteritems():
+        print key, value
+    plt.legend()
+    plt.show()
---
+++ probabilty_calibration/p_calibrate.py
@@ -0,0 +1,9 @@
+from sklearn.calibration import calibration_curve
+from sklearn.calibration import CalibratedClassifierCV
+import matplotlib.pyplot as plt
+import numpy as np
+
+# take a model / pipeline
+# perform isotonic regression
+# perform platt scaling
+# plot reliability curve
